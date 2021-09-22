05c7f6412f6c4b95723d9611b6a9180aeba079b1
padmini v <padmini.v@perfios.com>
2021-09-22 13:31:38 +0530

    ADDED: initialized project structure

---
+++ src/main/java/CabInvoiceGenerator/InvoiceGenerator.java
@@ -0,0 +1,8 @@
+package CabInvoiceGenerator;
+
+public class InvoiceGenerator 
+{
+
+	
+
+}
---
+++ src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
@@ -0,0 +1,11 @@
+package CabInvoiceGenerator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest 
+{
+	
+}
