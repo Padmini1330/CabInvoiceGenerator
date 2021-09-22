86f4f06a0f1e8bed93f32e2b0049645a9ea21dff
padmini v <padmini.v@perfios.com>
2021-09-22 15:45:14 +0530

    ADDED: class to maintain Invoice summary details

---
+++ src/main/java/CabInvoiceGenerator/InvoiceSummary.java
@@ -0,0 +1,29 @@
+package CabInvoiceGenerator;
+
+public class InvoiceSummary 
+{
+
+	private int numOfRides;
+	private double totalFare;
+	private Object averageFare;
+
+	public InvoiceSummary(int numOfRides, double totalFare) 
+	{
+		this.numOfRides=numOfRides;
+		this.totalFare=totalFare;
+		this.averageFare=this.totalFare/this.numOfRides;
+	}
+
+	public int getNumOfRides() {
+		return numOfRides;
+	}
+
+	public double getTotalFare() {
+		return totalFare;
+	}
+
+	public Object getAverageFare() {
+		return averageFare;
+	}
+	
+}
