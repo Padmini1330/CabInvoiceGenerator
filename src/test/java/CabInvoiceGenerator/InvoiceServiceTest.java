84c7e40e704581cda2dc3128dca0ef520723c588
padmini v <padmini.v@perfios.com>
2021-09-22 13:57:33 +0530

    REFACTORED: added test method to verify the fare when less than minimum fare

--- src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
@@ -23,6 +23,7 @@ public class InvoiceServiceTest
 		double distance=0.1;
 		int time=1;
 		double fare=invoiceGenerator.calculateFare(distance,time);
-		Assert.assertEquals(2, fare, 0.0);
+		Assert.assertEquals(5, fare, 0.0);
 	}
+	
 }
