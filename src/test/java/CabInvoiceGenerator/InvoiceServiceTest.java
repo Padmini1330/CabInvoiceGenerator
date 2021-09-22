b73f6e94f68a22fe640936310cca1d7a2e9a96a8
padmini v <padmini.v@perfios.com>
2021-09-22 13:36:07 +0530

    ADDED: test method to verify the generated fare

--- src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
@@ -7,5 +7,14 @@ import org.junit.Test;
 
 public class InvoiceServiceTest 
 {
+	InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+	@Test
+	public void givenDistanceAndTime_WhenProper_ShouldReturnTotalFare() 
+	{
+		double distance=2.0;
+		int time=5;
+		double fare=invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
 	
 }
